b7e17ce7146eece7a1eba18e2a8906d42f1236ee
Giovanni <geoseve@hotmail.com>
2021-06-11 14:29:03 -0600

    actaliza7

--- BaseDatos/dbo/Tablas/MarcaVehiculo_1.sql
+++ BaseDatos/dbo/Tablas/MarcaVehiculo_1.sql
@@ -1,4 +1,10 @@
 ﻿CREATE TABLE [dbo].[MarcaVehiculo]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	MarcaVehiculoID INT NOT NULL  IDENTITY (1,1) CONSTRAINT PK_MarcaVehiculo PRIMARY KEY CLUSTERED (MarcaVehiculoID)
+	, Descripcion VARCHAR (250) NOT NULL
+	, Estado BIT NOT NULL
+
 )
+	WITH (DATA_COMPRESSION = PAGE)
+	GO
+
