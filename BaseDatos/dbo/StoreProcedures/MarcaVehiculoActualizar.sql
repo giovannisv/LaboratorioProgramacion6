400790d54f6bfc4186dff576afb4d97555e24dcd
Giovanni <geoseve@hotmail.com>
2021-06-13 10:26:03 -0600

    actualizaractualizar

---
+++ BaseDatos/dbo/StoreProcedures/MarcaVehiculoActualizar.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[MarcaVehiculoActualizar]
+	@Descripcion VARCHAR (250)
+	, @Estado BIT
+	AS
+	BEGIN 
+	SET NOCOUNT ON
+	BEGIN Transaction TRASA
+	BEGIN 	TRY --Metodos
+	UPDATE MarcaVehiculo
+	SET 
+	Descripcion=@Descripcion,
+	Estado=@Estado
+	WHERE
+	MarcaVehiculoID = @MarcaVehiculoID
+
+	COMMIT TRANSACTION TRASA
+	SELECT 0 AS CodeError, '' AS MsgError
+	END TRY
+	BEGIN CATCH
+
+	SELECT
+	ERROR_NUMBER() AS CodeError
+	, ERROR_MESSAGE() AS MsgError
+	ROLLBACK TRANSACTION TRASA
+	END CATCH
+	END
