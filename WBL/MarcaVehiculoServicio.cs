d3c00cf9b89844a3d43454e9b4df9333c3e5bf0f
Giovanni <geoseve@hotmail.com>
2021-06-15 02:52:07 -0600

    actualizarwl

---
+++ WBL/MarcaVehiculoServicio.cs
@@ -0,0 +1,16 @@
+﻿
+using BD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public class MarcaVehiculoServicio
+    {
+        public MarcaVehiculoServicio(IDataAcces)
+        
+    }
+}
