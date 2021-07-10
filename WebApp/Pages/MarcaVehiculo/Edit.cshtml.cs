66e9a69969c892e20039176a5f1c3b0d41eb5dde
Giovanni <geoseve@hotmail.com>
2021-07-10 04:06:48 -0600

    actualizar4

--- WebApp/Pages/MarcaVehiculo/Edit.cshtml.cs
+++ WebApp/Pages/MarcaVehiculo/Edit.cshtml.cs
@@ -17,7 +17,56 @@ namespace WebApp.Pages.MarcaVehiculo
         {
             this.marcaVehiculoServicio = marcaVehiculoServicio;
         }
+        [BindProperty]
+        public MarcaVehiculoEntity Entity { get; set; } = new MarcaVehiculoEntity();    
+        [BindProperty (SupportsGet =true)]
+        public int ? id { get; set; }
 
-        public  int 
+        public async Task<IActionResult>OnGet()
+        {
+            try
+            {
+                if(id.HasValue)
+                {
+                    Entity = await marcaVehiculoServicio.GetByID(new() { MarcaVehiculoID = id });
+                }
+                return Page();
+            }
+            catch (Exception ex)
+            {
+
+                return Content(content:ex.Message);
+            }
+         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                if (Entity.MarcaVehiculoID.HasValue)
+                {
+                    //actualizar
+                    var result= await marcaVehiculoServicio.Update(Entity);
+                    if (result.CodeError != 0) throw new Exception(message: result.MsgError);
+                    TempData["Msg"] = "Se actualizo correctamente";
+                }
+
+                else
+                {
+
+                    //nuevo
+                    var result = await marcaVehiculoServicio.Create(Entity);
+                    if (result.CodeError != 0) throw new Exception(message: result.MsgError);
+                    TempData["Msg"] = "Se agrego correctamente";
+
+                }
+                return RedirectToPage("Grid");
+            }
+            catch (Exception ex)
+            {
+
+                return Content(content: ex.Message);
+            }
+        }
     }
 }
