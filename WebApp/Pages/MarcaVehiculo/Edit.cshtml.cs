3426c54e41410c69b3f0c139c9e794b7ab458e81
Giovanni <geoseve@hotmail.com>
2021-07-10 02:42:22 -0600

    actualizar3

--- WebApp/Pages/MarcaVehiculo/Edit.cshtml.cs
+++ WebApp/Pages/MarcaVehiculo/Edit.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WBL;
+using Entity;
 
 namespace WebApp.Pages.MarcaVehiculo
 {
     public class EditModel : PageModel
     {
-        
+        private readonly IMarcaVehiculoServicio marcaVehiculoServicio;
 
-        
+        public EditModel(IMarcaVehiculoServicio marcaVehiculoServicio)
+        {
+            this.marcaVehiculoServicio = marcaVehiculoServicio;
+        }
+
+        public  int 
     }
 }
