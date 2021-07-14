3ad792bd6013f71bc90ce996dec7e4ef9f74f413
Giovanni <geoseve@hotmail.com>
2021-07-13 23:49:59 -0600

    actualize6

---
+++ Entity/dbo/VehiculoEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.dbo
+{
+    public class VehiculoEntity:EN
+    {
+        public int? VehiculoId { get; set; }
+
+        public int? MarcaVehiculoID { get; set; }
+
+        public virtual MarcaVehiculoEntity MarcaVehiculo { get; set; }
+
+        public string Matricula { get; set; }
+        public string Color { get; set; }
+        public string Modelo { get; set; }
+        public DateTime FechaModelo { get; set; }
+        public bool TieneDefectos { get; set; }
+        public string Defectos { get; set; }
+        
+
+
+
+    }
+}
