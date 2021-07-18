7cfd802929e2ebbfbc79fc322d23507a57b0bed9
Giovanni <geoseve@hotmail.com>
2021-07-18 14:55:27 -0600

    actualiza13

---
+++ Entity/dbo/AgenciasEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+     public class AgenciasEntity
+    {
+        public AgenciasEntity()
+        {
+            Provincia = Provincia ?? new CatalogoProvinciaEntity();
+            Canton = Canton ?? new CatalogoCantonEntity();
+            Distrito = Distrito ?? new CatalogoDistritoEntity();
+        }
+
+        public int? AgenciaId { get; set; }
+        public string Nombre { get; set; }
+        public int? IdCatalogoProvincia { get; set; }
+        public CatalogoProvinciaEntity Provincia { get; set; }
+        public int? IdCatalogoCanton { get; set; }
+        public CatalogoCantonEntity Canton { get; set; }
+        public int? IdCatalogoDistrito { get; set; }
+        public CatalogoDistritoEntity Distrito { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ Entity/dbo/CatalogoDistritoEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class CatalogoDistritoEntity
+    {
+        public CatalogoDistritoEntity()
+        {
+            CatalogoCanton = CatalogoCanton ?? new CatalogoCantonEntity();
+            //?? validacion son signos de pregunta
+        }
+
+        public int? IdCatalogoDistrito { get; set; }
+        public string NombreCatalogoDistrito { get; set; }
+        public int? IdCatalogoCanton { get; set; }
+        //llave foranea
+        public CatalogoCantonEntity CatalogoCanton { get; set; }
+
+    }
+}
