c26de395c80f57c3f92a6d747b66465ad5510540
Giovanni <geoseve@hotmail.com>
2021-07-19 16:46:20 -0600

    actualiza3

---
+++ WBL/AgenciaServicio.cs
@@ -0,0 +1,126 @@
+﻿using BD;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBL
+{
+    public class AgenciaServicio
+    {
+
+        private readonly IDataAcces sql;
+
+        public AgenciaServicio(IDataAcces _sql)
+        {
+            sql = _sql;
+        }
+        public async Task<IEnumerable<AgenciasEntity>> Get()
+        {
+
+            try
+            {
+                var result = sql.QueryAsync<AgenciasEntity,
+                    CatalogoProvinciaEntity,
+                    CatalogoCantonEntity,
+                    CatalogoDistritoEntity
+
+                    >("AgenciaObtener", "IdCatalogoProvincia,IdCatalogoCanton,IdCatalogoDistrito");
+                return await result;
+
+            }
+            catch (Exception EX)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<AgenciasEntity> GetByID(AgenciasEntity entity)
+        {
+            try
+            {
+                var result = sql.QueryFirstAsync<AgenciasEntity>("AgenciaObtener", new
+                {
+                    entity.AgenciaId
+                }
+                    );
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+        public async Task<DBEntity> Create(AgenciasEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("AgenciaInsertar", new
+                {
+                    entity.Nombre,
+                    entity.IdCatalogoProvincia,
+                    entity.IdCatalogoCanton,
+                    entity.IdCatalogoDistrito,
+                    entity.Estado
+                }
+                    );
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public async Task<DBEntity> Update(AgenciasEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("AgenciaActualizar", new
+                {
+                    entity.AgenciaId,
+                    entity.Nombre,
+                    entity.IdCatalogoProvincia,
+                    entity.IdCatalogoCanton,
+                    entity.IdCatalogoDistrito,
+                    entity.Estado
+                }
+                    );
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public async Task<DBEntity> Delete(AgenciasEntity entity)
+        {
+            try
+            {
+                var result = sql.ExecuteAsync("AgenciaEliminar", new
+                {
+                    entity.AgenciaId,
+
+                }
+                    );
+                return await result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
